916cedb58280976e6f89f8f078157422124066b6
AidenColin024 <1212500@student.roc-nijmegen.nl>
2025-06-24 12:59:03 +0200

    admin pagina's toevoegen

---
+++ html/Admin/admin-hotel.php
@@ -0,0 +1,29 @@
+
+<!DOCTYPE html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Polar & Paradise</title>
+    <link rel="stylesheet" href="../vakantie.css?v=<?= time() ?>">
+
+
+</head>
+<body>
+
+<!-- HEADER -->
+<header class="pp-header">
+    <div class="logo">
+        <a href="../index.php"><img src="/images/image1 (1).png" alt="Polar & Paradise"></a>
+    </div>
+    <nav class="pp-nav">
+        <ul>
+            <li><a href="admin.php">Home</a></li>
+            <li><a href="admin-vragen.php">Inkomende vragen</a></li>
+            <li><a href="admin-recensies.php">Inkomende reviews</a></li>
+            <li><a href="admin-land.php">Landen</a></li>
+            <li><a href="admin-hotel.php">Hotels</a></li>
+            <li><a href="../uitlog.php">Uitloggen</a></li>
+        </ul>
+    </nav>
+</header>
